3ebf13b46694d7ad7dd21e83d3ffab00d0f7db72
SzymonJ99 <szymon.j@hotmail.com>
2019-10-30 18:46:16 +0000

    Added basic movement.

--- Week2/InputAndControl/InputAndControl.cpp
+++ Week2/InputAndControl/InputAndControl.cpp
@@ -14,6 +14,22 @@ void main()
 
 	/**** Set up your scene here ****/
 
+	// Create mesh and model objects.
+	IMesh* cubeMesh;
+	IModel* cube;
+
+	// Set meshes.
+	cubeMesh = myEngine->LoadMesh("Cube.x");
+
+	// Create models.
+	cube = cubeMesh->CreateModel();
+
+	// Create FPS-Style camera.
+	ICamera* myCamera;
+	myCamera = myEngine->CreateCamera(kFPS);
+
+	// Define variables.
+	const double moveSpeed = 0.05;
 
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
@@ -23,6 +39,47 @@ void main()
 
 		/**** Update your scene each frame here ****/
 
+		// As the computer we are running this on is too fast for TLEngine, we add delay.
+		for (int delay = 0; delay < 1000000; delay++) { /* empty body */ }
+
+		// Keyboard control.
+		// Exit game.
+		if (myEngine->KeyHit(Key_Escape))
+		{
+			myEngine->Stop();
+		}
+		// Move the cube with WASDQE keys. 
+		// The movement is relative to the screen at 0,0,0 not the camera.
+		if (myEngine->KeyHeld(Key_W))
+		{
+			// Move cube into the screen.
+			cube->MoveZ(moveSpeed);
+		}
+		if (myEngine->KeyHeld(Key_S))
+		{
+			// Move cube away from the screen.
+			cube->MoveZ(-moveSpeed);
+		}
+		if (myEngine->KeyHeld(Key_A))
+		{
+			// Move cube left.
+			cube->MoveX(-moveSpeed);
+		}
+		if (myEngine->KeyHeld(Key_D))
+		{
+			// Move cube right.
+			cube->MoveX(moveSpeed);
+		}
+		if (myEngine->KeyHeld(Key_Q))
+		{
+			// Move cube up.
+			cube->MoveY(moveSpeed);
+		}
+		if (myEngine->KeyHeld(Key_E))
+		{
+			// Move cube down.
+			cube->MoveY(-moveSpeed);
+		}
 	}
 
 	// Delete the 3D engine now we are finished with it
