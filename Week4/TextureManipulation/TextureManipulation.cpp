ee050bf15a99a26ae1454d5983b763697f3d1856
SzymonJ99 <szymon.j@hotmail.com>
2019-11-01 17:27:55 +0000

    Changed texture on a sphere.

--- Week4/TextureManipulation/TextureManipulation.cpp
+++ Week4/TextureManipulation/TextureManipulation.cpp
@@ -3,6 +3,10 @@
 #include <TL-Engine.h>	// TL-Engine include file and namespace
 using namespace tle;
 
+// Create some variables
+const float kCameraSpeed = 0.05f;
+const float kCameraRotation = 0.05f;
+
 void main()
 {
 	// Create a 3D engine (using TLX engine here) and open a window for it
@@ -14,6 +18,21 @@ void main()
 
 	/**** Set up your scene here ****/
 
+	// Create mesh and model objects
+	IMesh* sphereMesh;
+	IModel* sphere;
+
+	// Change mesh
+	sphereMesh = myEngine->LoadMesh("Sphere.x");
+
+	// Create model
+	sphere = sphereMesh->CreateModel();
+
+	// Create camera
+	const ICamera* myCamera = myEngine->CreateCamera(kFPS);
+
+	// Change sphere texture
+	sphere->SetSkin("Jupiter.jpg");
 
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
