e0c0a6ce0cbfa50c10e3b0e886a17ea4376d30e5
SzymonJ99 <szymon.j@hotmail.com>
2019-11-02 02:22:08 +0000

    Removed sphere and cube. Added controllable arrow.

--- Week4/TextureManipulation/TextureManipulation.cpp
+++ Week4/TextureManipulation/TextureManipulation.cpp
@@ -24,35 +24,49 @@ std::chrono::system_clock::time_point timePointA = std::chrono::system_clock::no
 std::chrono::system_clock::time_point timePointB = std::chrono::system_clock::now();
 
 // START OF FPS LIMITER CONSTANTS
-const double desiredFPS = GetDeviceCaps(GetDC(NULL), VREFRESH); // Get the screen's refresh rate.
+const double desiredFPS = GetDeviceCaps(GetDC(nullptr), VREFRESH); // Get the screen's refresh rate. Using nullptr gets the refreshrate of the main display.
 const double milliseconds = 1000.0;
 const double frameTime = milliseconds / desiredFPS;
 // END OF FPS LIMITER CONSTANTS
 
 // Create some constants
-const float kCameraSpeed = 0.05f;
-const float kCameraRotation = 0.1f;
-const int minX = -40;
-const int maxX = 40;
-const float moveSpeed = 0.05;
-const float rotationSpeed = 0.075;
-const int initialSpeedMultiplier = 240;
+const float moveSpeed = 0.20;
+const int initialSpeedMultiplier = 480;
 const int speedMultiplier = initialSpeedMultiplier / desiredFPS;
 
-// Get the sphere texture based on whether or not it's cloudy.
-string GetSphereTexture(bool isCloudy)
+// Function to move object with WASDQEZX keys.
+void MoveObject(I3DEngine* myEngine, IModel* object)
 {
-	// Set up cloudy and clear texture.
-	string cloudy = "Clouds.jpg";
-	string clear = "EarthPlain.jpg";
-
-	if (isCloudy)
+	// The movement is relative.
+	if (myEngine->KeyHeld(Key_W))
 	{
-		return cloudy;
+		// Move object into the screen.
+		object->MoveLocalZ(moveSpeed * speedMultiplier);
 	}
-	else
+	if (myEngine->KeyHeld(Key_S))
 	{
-		return clear;
+		// Move object away from the screen.
+		object->MoveLocalZ(-moveSpeed * speedMultiplier);
+	}
+	if (myEngine->KeyHeld(Key_A))
+	{
+		// Rotate object left.
+		object->RotateLocalY(-moveSpeed * speedMultiplier);
+	}
+	if (myEngine->KeyHeld(Key_D))
+	{
+		// Rotate object right.
+		object->RotateLocalY(moveSpeed * speedMultiplier);
+	}
+	if (myEngine->KeyHeld(Key_Q))
+	{
+		// Rotate object up.
+		object->RotateLocalX(moveSpeed * speedMultiplier);
+	}
+	if (myEngine->KeyHeld(Key_E))
+	{
+		// Rotate object down.
+		object->RotateLocalX(-moveSpeed * speedMultiplier);
 	}
 }
 
@@ -71,31 +85,22 @@ void main()
 	/**** Set up your scene here ****/
 
 	// Create mesh and model objects
-	IMesh* sphereMesh;
-	IModel* sphere;
 	IMesh* gridMesh;
 	IModel* grid;
+	IMesh* arrowMesh;
+	IModel* arrow;
 
 	// Set meshes
-	sphereMesh = myEngine->LoadMesh("Sphere.x");
 	gridMesh = myEngine->LoadMesh("Grid.x");
+	arrowMesh = myEngine->LoadMesh("Arrow.x");
 
 	// Create models
-	int sphereHeightPos = 10;
-	sphere = sphereMesh->CreateModel(0, sphereHeightPos, 0);
 	grid = gridMesh->CreateModel();
+	arrow = arrowMesh->CreateModel(0, 10, 0);
 
-	// Create camera
+	// Create camera and attach to arrow.
 	ICamera* myCamera = myEngine->CreateCamera(kManual);
-
-	// Change sphere texture
-	sphere->SetSkin("EarthPlain.jpg");
-
-	// Set is sphere cloudy
-	bool sphereCloudy = false;
-
-	// Set direction of sphere
-	static string sphereDirection = "right";
+	myCamera->AttachToParent(arrow);
 
 	// Define is the game paused.
 	bool isPaused = false;
@@ -110,31 +115,31 @@ void main()
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
 	{
-		// Try and fix messed up camera
-		// Rotate camera based on mouse input
-		myCamera->RotateLocalY(myEngine->GetMouseMovementX() * kCameraRotation);
-		myCamera->RotateLocalX(myEngine->GetMouseMovementY() * kCameraRotation);
-
 		// START OF FPS LIMITER CODE ///////////////////////////////////
 		// Set the timePointA to the time right now
 		timePointA = std::chrono::system_clock::now();
 		// Create a work_time double of type duration, measured in milliseconds (double data type), and set it to the time difference between timePointA and timePointB.
-		std::chrono::duration<double, std::milli> work_time = timePointA - timePointB;
+		std::chrono::duration<double, std::milli> const work_time = timePointA - timePointB;
 
 		if (work_time.count() < frameTime)
 		{
 			// Set the delta_ms duration
-			std::chrono::duration<double, std::milli> delta_ms(frameTime - work_time.count());
+			std::chrono::duration<double, std::milli> const delta_ms(frameTime - work_time.count());
 			auto delta_ms_duration = std::chrono::duration_cast<std::chrono::milliseconds>(delta_ms);
 			std::this_thread::sleep_for(std::chrono::milliseconds(delta_ms_duration.count()));
 		}
 
 		timePointB = std::chrono::system_clock::now();
-		std::chrono::duration<double, std::milli> sleep_time = timePointB - timePointA;
+		std::chrono::duration<double, std::milli> const sleep_time = timePointB - timePointA;
 		// END OF FPS LIMITER CODE ///////////////////////////////////
 
 		if (!isPaused)
 		{
+			// Try and fix messed up camera
+			// Rotate camera based on mouse input
+			//myCamera->RotateLocalY(myEngine->GetMouseMovementX() * kCameraRotation);
+			//myCamera->RotateLocalX(myEngine->GetMouseMovementY() * kCameraRotation);
+
 			// Draw the scene
 			myEngine->DrawScene();
 
@@ -144,41 +149,6 @@ void main()
 			myFont->Draw(to_string(totalFrames), 0, 0);
 
 			/**** Update your scene each frame here ****/
-
-			// Bounce sphere between X = minX and X = maxX, roll, and change texture;
-			if (sphereDirection == "right")
-			{
-				// If the sphere is below the maxX coordinate
-				if (sphere->GetX() < maxX)
-				{
-					// Rotate and move towards maxX.
-					sphere->RotateZ(-rotationSpeed * speedMultiplier);
-					sphere->MoveX(moveSpeed);
-				}
-				else
-				{
-					// Toggle the state of sphereCloudy, change texture and reverse direction.
-					sphereCloudy = !sphereCloudy;
-					sphere->SetSkin(GetSphereTexture(sphereCloudy));
-					sphereDirection = "left";
-				}
-			}
-			else
-			{
-				if (sphere->GetX() > minX)
-				{
-					// Rotate and move towards minX.
-					sphere->RotateZ(rotationSpeed * speedMultiplier);
-					sphere->MoveX(-moveSpeed);
-				}
-				else
-				{
-					// Toggle the state of sphereCloudy and change texture.
-					sphereCloudy = !sphereCloudy;
-					sphere->SetSkin(GetSphereTexture(sphereCloudy));
-					sphereDirection = "right";
-				}
-			}
 
 			// Control keyboard input.
 			// Exit game.
@@ -204,12 +174,16 @@ void main()
 				}
 				isMouseCaptured = !isMouseCaptured;
 			}
+			MoveObject(myEngine, arrow);
 		}
 		else
 		{
 			// Toggle isPaused.
 			if (myEngine->KeyHit(Key_P))
 			{
+				// To prevent the camera from moving when resuming game, call GetMouseMovementX/Y()
+				myEngine->GetMouseMovementX();
+				myEngine->GetMouseMovementY();
 				isPaused = !isPaused;
 			}
 		}
